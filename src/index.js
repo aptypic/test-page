7936142b165dae2bbaef0bf2adeb7a8b2bc57823
asivolap <artur.sivolap@haiku.dev>
2023-05-04 01:38:37 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDom.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//       <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
